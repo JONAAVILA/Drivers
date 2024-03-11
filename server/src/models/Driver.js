48046a2dbdb989c2844dc64eb6129c52a3a9c622
JONAAVILA <jona_1969@hotmail.com>
2024-03-11 18:17:08 -0300

    DB ready

--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -1,12 +1,32 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
-  // defino el modelo
   sequelize.define('Driver', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    lasName: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    nationality: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    release: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  },{
+    timestamps: false
   });
 };

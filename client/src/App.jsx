82ae583cbb88313cb57035af7f87e725c6c30f6d
JONAAVILA <jona_1969@hotmail.com>
2024-03-13 12:31:20 -0300

    components and views created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
-import Landing from './views/Landing';
+import Landing from './views/landing/Landing';
+import Home from './views/home/Home';
+import Detail from './views/detail/Detail';
+import Form from './views/form/Forma';
 
 function App() {
 
@@ -8,6 +11,9 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Landing/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/detail' element={<Detail/>} />
+        <Route path='/form' element={<Form/>} />
       </Routes>
     </>
   )
--- client/src/views/landing/Landing.jsx
+++ client/src/views/landing/Landing.jsx
@@ -1,21 +1,23 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { allDrivers } from "../redux/Actions";
+import { useDispatch } from 'react-redux';
+import { allDrivers } from "../../redux/Actions";
+import { Link } from "react-router-dom";
 
 
 const Landing = ()=>{
 
     const dispatch = useDispatch();
-    const state = useSelector(state => state.drivers)
 
     useEffect(()=>{
         dispatch(allDrivers())
     },[])
 
-    console.log(state)
     return(
         <div>
             <h1>Welcome</h1>
+            <Link to='/home' >
+                <button>enter</button>
+            </Link>
         </div>
     )
 }

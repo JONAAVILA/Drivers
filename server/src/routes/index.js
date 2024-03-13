7c982c6323b69d5d38d3db615343ac3e0c4601e6
JONAAVILA <jona_1969@hotmail.com>
2024-03-13 10:51:08 -0300

    get all teams to db complete

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,12 +3,14 @@ const getAllDrivers = require('./../controllers/get/getAllDrivers');
 const getDriverById = require('./../controllers/get/getDriverById');
 const getDriverByName = require('./../controllers/get/getDriverByName');
 const postDriver = require('./../controllers/post/postDriver');
+const getAllTeams = require('./../controllers/get/getAllTeams');
 
 const router = Router();
 
 router.get('/drivers/name', getDriverByName)
 router.get('/drivers/:idDriver', getDriverById)
 router.get('/drivers', getAllDrivers)
+router.get('/teams', getAllTeams)
 
 router.post('/drivers/create', postDriver)
 

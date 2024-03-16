51a11658c09c812ac4132023d2d91980de5fdfef
JONAAVILA <jona_1969@hotmail.com>
2024-03-16 12:48:43 -0300

    order reducer complete

--- client/src/redux/Reducer.js
+++ client/src/redux/Reducer.js
@@ -27,11 +27,11 @@ const rootReducer = (state = initialState, action) =>{
             if(!state.drivers.db){
                    const filterOrder = [...state.drivers.api]
                 if (action.payload === "A") {
-                    filterOrder.sort((a, b) => a.name.localeCompare(b.name))
+                    filterOrder.sort((a, b) => a.name.forename.localeCompare(b.name.forename))
                 }else if(action.payload === "Random"){
                     filterOrder.sort(() => Math.random() - 0.5)
                 }else{
-                    filterOrder.sort((a, b) => b.name.localeCompare(a.name))
+                    filterOrder.sort((a, b) => b.name.forename.localeCompare(a.name.forename))
                 }
                 return {
                     ...state,
@@ -40,11 +40,11 @@ const rootReducer = (state = initialState, action) =>{
             }else{
                 const filterOrder = [...state.drivers.api,...state.drivers.db]
                 if (action.payload === "A") {
-                    filterOrder.sort((a, b) => a.name.localeCompare(b.name))
+                    filterOrder.sort((a, b) => a.name.forename.localeCompare(b.name.forename))
                 }else if(action.payload === "Random"){
                     filterOrder.sort(() => Math.random() - 0.5)
                 }else{
-                    filterOrder.sort((a, b) => b.name.localeCompare(a.name))
+                    filterOrder.sort((a, b) => b.name.forename.localeCompare(a.name.forename))
                 }
                 return {
                     ...state,

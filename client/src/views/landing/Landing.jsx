5da00992ac08e2fbfcc258739127c429b1531094
JONAAVILA <jona_1969@hotmail.com>
2024-03-19 18:47:11 -0300

    style landing

--- client/src/views/landing/Landing.jsx
+++ client/src/views/landing/Landing.jsx
@@ -4,10 +4,12 @@ import './Landing.css';
 const Landing = ()=>{
     return(
         <div className="box_landing" >
+            <div className="box_tittle_landing" >
             <h1>Welcome</h1>
-            <Link to='/home' >
-                <button>enter</button>
-            </Link>
+                <Link to='/home' >
+                  <button>enter</button>
+                </Link>
+            </div>
         </div>
     )
 }

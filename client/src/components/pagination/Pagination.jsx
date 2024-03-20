7d32ee0bcee146ad0e427772f434db9349afdaba
JONAAVILA <jona_1969@hotmail.com>
2024-03-19 23:08:58 -0300

    cards created

--- client/src/components/pagination/Pagination.jsx
+++ client/src/components/pagination/Pagination.jsx
@@ -18,10 +18,34 @@ const Pagination = ()=>{
                {drivers.api?.map(driver => {
                     return(
                         <Link to={`/detail/api/${driver.id}`} >
-                            <div className="box_card" key={driver.id} >
-                                {driver.image.url?(<img src={driver.image.url} alt="" />):(<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-user-scan"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 9a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M4 8v-2a2 2 0 0 1 2 -2h2" /><path d="M4 16v2a2 2 0 0 0 2 2h2" /><path d="M16 4h2a2 2 0 0 1 2 2v2" /><path d="M16 20h2a2 2 0 0 0 2 -2v-2" /><path d="M8 16a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2" /></svg>)}
+                            <div className="box_card" key={driver.id}>
+                                <div className="image_back_profile" style={{backgroundImage:`url(${driver.image.url})`,
+                                                                            backgroundSize: 'cover',
+                                                                            backgroundRepeat: 'no-repeat',
+                                                                            backgroundPosition: 'center', 
+                                                                        }} ></div>
+                                {driver.image.url?(<img src={driver.image.url} alt="" />):(<svg  xmlns="http://www.w3.org/2000/svg"
+                                                                                                 width="100" 
+                                                                                                 height="100"
+                                                                                                 viewBox="0 0 24 24"  
+                                                                                                 fill="none"  
+                                                                                                 stroke="currentColor"  
+                                                                                                 stroke-width="2"  
+                                                                                                 stroke-linecap="round"  
+                                                                                                 stroke-linejoin="round"  
+                                                                                                 >
+                                                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                                                <path d="M10 9a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                                                                                                <path d="M4 8v-2a2 2 0 0 1 2 -2h2" />
+                                                                                                <path d="M4 16v2a2 2 0 0 0 2 2h2" />
+                                                                                                <path d="M16 4h2a2 2 0 0 1 2 2v2" />
+                                                                                                <path d="M16 20h2a2 2 0 0 0 2 -2v-2" />
+                                                                                                <path d="M8 16a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2" />
+                                                                                            </svg>)}
                                 <h1>{driver.name.forename}</h1>
-                                <p>{driver.teams}</p>
+                                <div className="box_teams">
+                                    <p>{driver.teams}</p>
+                                </div>
                             </div>
                         </Link>
                     )

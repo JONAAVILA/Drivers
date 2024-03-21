99e721f95bc01cdd07ed3e205d46e5d3e4431337
JONAAVILA <jona_1969@hotmail.com>
2024-03-21 09:28:44 -0300

    pagination created

--- client/src/components/pagination/Pagination.jsx
+++ client/src/components/pagination/Pagination.jsx
@@ -22,10 +22,14 @@ const Pagination = ()=>{
         }
     }
 
+    const handleNext  = (event)=>{
+        if(event) setCurrentPage(currentPage + 1)
+    }
+
     return( 
         <div className="conteiner_page" >
             <div className="box_page" >
-               {drivers?.map(driver => {
+               {drivers?.slice(startIndex,endIndex).map(driver => {
                     return(
                         <Link to={`/detail/api/${driver.id}`} >
                             <div className="box_card" key={driver.id}>
@@ -59,9 +63,13 @@ const Pagination = ()=>{
                })} 
             </div>
             <div className="box_button_handlers" >
-                <svg onClick={handlePrev} xmlns="http://www.w3.org/2000/svg"  width="40"  height="40"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 6l-6 6l6 6" /></svg>
+                <div onClick={handlePrev} >
+                    <svg xmlns="http://www.w3.org/2000/svg"  width="25"  height="25"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 6l-6 6l6 6" /></svg>
+                </div>
                 <p>{currentPage}</p>
-                <svg  xmlns="http://www.w3.org/2000/svg"  width="40"  height="40"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg>
+                <div onClick={handleNext} >
+                    <svg xmlns="http://www.w3.org/2000/svg"  width="25"  height="25"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg>
+                </div>
             </div>
         </div>
     )

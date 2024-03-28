18bc42f0a573cfe00075226bf415022c7d708caf
JONAAVILA <jona_1969@hotmail.com>
2024-03-27 22:22:10 -0300

    detail

--- client/src/views/detail/Detail.jsx
+++ client/src/views/detail/Detail.jsx
@@ -8,13 +8,15 @@ const Detail = ()=>{
     const from = pathname.pathname.split("/").find(path => path === "api")
     const { id } = useParams()
     const driverFound = drivers.api.find(driver => driver.id === parseInt(id))
-
+    //cambiar modelo y stado del from de lasname -> name: {forename:"", surename:""}
+    console.log(driverFound)
     if(driverFound){
         return(
             <div className="box_detail">
                 <div className="box_info" >
                     <img src={driverFound.image.url} alt="" />
                     <h1>{driverFound.name.forename}</h1>
+                    <h2>{driverFound.name.surname}</h2>
                 </div>
             </div>
         )

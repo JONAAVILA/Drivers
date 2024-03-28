2267e3f70263b8779feae3ca6e5318616c714970
JONAAVILA <jona_1969@hotmail.com>
2024-03-28 09:58:25 -0300

    landing change background

--- client/src/views/landing/Landing.jsx
+++ client/src/views/landing/Landing.jsx
@@ -4,12 +4,10 @@ import './Landing.css';
 const Landing = ()=>{
     return(
         <div className="box_landing" >
-            <div className="box_tittle_landing" >
             <h1>Welcome</h1>
-                <Link to='/home' >
-                  <button>enter</button>
-                </Link>
-            </div>
+            <Link to='/home' >
+                <button>enter</button>
+            </Link>
         </div>
     )
 }
